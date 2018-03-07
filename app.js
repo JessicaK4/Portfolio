970961868a7e8dfeeb1e0904c7c29318470d9556
JessicaK4 <jessica.kemen@gmail.com>
2018-03-07 10:48:08 -0500

    message

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ const app=express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','.hbs');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/public',express.static(path.join(__dirname,'public')));
 
 app.get('/', function(req, res){
   res.render('index');
 });
-
+app.get('/views/contact', function(req, res){
+  res.render('contact');
+});
+app.get('/views/marketing', function(req, res){
+  res.render('marketing');
+});
+app.get('/views/design', function(req, res){
+  res.render('design');
+});
 app.listen(3000, function(){
   console.log('port3000');
 });
